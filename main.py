5cbe09f06cf8f6589ff8c6a830f805f49f874fd4
Ravi Gordon <84871171+rg460@users.noreply.github.com>
2022-07-18 16:19:50 +0100

    logic for creating love score done

--- main.py
+++ main.py
@@ -48,5 +48,37 @@ v = int(joined_str_lower_case.count("v"));
 e = int(joined_str_lower_case.count("e"));
 total_love = l + o + v + e;
 
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# # Plan
+#  combine names
+joined_str = name1 + name2;
+# guard against the user inputing names in a certain way 
+# turn joined_str into lower case
+joined_str_lower_case = joined_str.lower();
+#  check how many times  joined_str_lower_case has a t,r,u,e in it .
+t = (joined_str_lower_case.count("t"));
+r = (joined_str_lower_case.count("r"));
+u = (joined_str_lower_case.count("u"));
+e = (joined_str_lower_case.count("e"));
+# add together to get a total
+total_true = t + r + u + e;
+#  check how many times  joined_str_lower_case has a l,o,v,e in it .
+l = (joined_str_lower_case.count("l"));
+o = (joined_str_lower_case.count("0"));
+v = (joined_str_lower_case.count("v"));
+e = (joined_str_lower_case.count("e"));
+total_love = l + o + v + e;
+# calculate love score by concantenate the totals of true and love , convert both to strings
+love_score = str(total_true) + str(total_love);
+# print the users love score 
+print(f"Your Love Score is {love_score}")
+
+
 
 
