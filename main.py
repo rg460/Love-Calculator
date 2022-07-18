56f5c2f61f6773d57ab89b193b436d64005ffc3d
Ravi Gordon <84871171+rg460@users.noreply.github.com>
2022-07-18 16:04:55 +0100

    logic for how many times love appears in joint_str

--- main.py
+++ main.py
@@ -21,3 +21,32 @@ e = int(joined_str_lower_case.count("e"));
 total_true = t + r + u + e;
 print(total_true);
 
+## 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# # Plan
+#  combine names
+joined_str = name1 + name2;
+# guard against the user inputing names in a certain way 
+# turn joined_str into lower case
+joined_str_lower_case = joined_str.lower();
+#  check how many times  joined_str_lower_case has a t,r,u,e in it .
+t = int(joined_str_lower_case.count("t"));
+r = int(joined_str_lower_case.count("r"));
+u = int(joined_str_lower_case.count("u"));
+e = int(joined_str_lower_case.count("e"));
+# add together to get a total
+total_true = t + r + u + e;
+#  check how many times  joined_str_lower_case has a l,o,v,e in it .
+l = int(joined_str_lower_case.count("l"));
+o = int(joined_str_lower_case.count("0"));
+v = int(joined_str_lower_case.count("v"));
+e = int(joined_str_lower_case.count("e"));
+total_love = l + o + v + e;
+
+
+
