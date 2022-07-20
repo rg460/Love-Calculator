83198849043698e5f57ee7344ef0dfb11099aa54
Ravi Gordon <84871171+rg460@users.noreply.github.com>
2022-07-20 11:02:11 +0100

    type error fixed
    by making a int love score variable and using the created int variable in the if statements

--- main.py
+++ main.py
@@ -11,32 +11,29 @@ joined_str = name1 + name2;
 # guard against the user inputing names in a certain way 
 # turn joined_str into lower case
 joined_str_lower_case = joined_str.lower();
-#  create a count variable for TRUE
-count_TRUE = 0;
-#  check how many times  joined_str_lower_case has a T in it .
-t = int(joined_str_lower_case.count("t"));
-r = int(joined_str_lower_case.count("r"));
-u = int(joined_str_lower_case.count("u"));
-e = int(joined_str_lower_case.count("e"));
+#  check how many times  joined_str_lower_case has a t,r,u,e in it .
+t = (joined_str_lower_case.count("t"));
+r = (joined_str_lower_case.count("r"));
+u = (joined_str_lower_case.count("u"));
+e = (joined_str_lower_case.count("e"));
+# add together to get a total
 total_true = t + r + u + e;
-print(total_true);
 #  check how many times  joined_str_lower_case has a l,o,v,e in it .
-l = int(joined_str_lower_case.count("l"));
-o = int(joined_str_lower_case.count("0"));
-v = int(joined_str_lower_case.count("v"));
-e = int(joined_str_lower_case.count("e"));
+l = (joined_str_lower_case.count("l"));
+o = (joined_str_lower_case.count("0"));
+v = (joined_str_lower_case.count("v"));
+e = (joined_str_lower_case.count("e"));
 total_love = l + o + v + e;
 # calculate love score by concantenate the totals of true and love , convert both to strings
 love_score = str(total_true) + str(total_love);
+int_love_score = int(love_score)
 # if statement to dertemine the users love score and phrase to go with love score
-if (love_score < 10) or (love_score > 90):
+if (int_love_score < 10) or (int_love_score > 90):
   print(f"Your score is {love_score}, you go together like coke and mentos.")
-elif (love_score >= 40) and (love_score <= 50):
+elif (int_love_score >= 40) and (int_love_score <= 50):
   print(f"Your score is {love_score}, you are alright together.")
 else:
   print(f"Your score is {love_score}.")
-
-
-
+    
 
 
